f6f551edf97ad47b05ace09aa13d8b67b90752bd
wsq <wsq1275093443@gmail.com>
2025-08-18 01:48:14 +0800

    fix: for ci warning

--- engine/src/core/logger/logger.h
+++ engine/src/core/logger/logger.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "common/export.h"
 #include <spdlog/spdlog.h>
-#include <memory>
 
 namespace Comet {
     class COMET_API Logger {
@@ -28,39 +27,40 @@ namespace Comet {
 
 #define LOG_ERROR(fmt, ...)                                                            \
     do {                                                                               \
-        if (auto logger = ::Comet::Logger::get_console_logger()) {                  \
+        if (auto logger = ::Comet::Logger::get_console_logger()) {                     \
             logger->error(fmt, ##__VA_ARGS__);                                         \
         }                                                                              \
     } while (0)
 #define LOG_WARN(fmt, ...)                                                             \
     do {                                                                               \
-        if (auto logger = ::Comet::Logger::get_console_logger()) {                  \
+        if (auto logger = ::Comet::Logger::get_console_logger()) {                     \
             logger->warn(fmt, ##__VA_ARGS__);                                          \
         }                                                                              \
     } while (0)
 #define LOG_DEBUG(fmt, ...)                                                            \
     do {                                                                               \
-        if (auto logger = ::Comet::Logger::get_console_logger()) {                  \
+        if (auto logger = ::Comet::Logger::get_console_logger()) {                     \
             logger->debug(fmt, ##__VA_ARGS__);                                         \
         }                                                                              \
     } while (0)
 #define LOG_INFO(fmt, ...)                                                             \
     do {                                                                               \
-        if (auto logger = ::Comet::Logger::get_console_logger()) {                  \
+        if (auto logger = ::Comet::Logger::get_console_logger()) {                     \
             logger->info(fmt, ##__VA_ARGS__);                                          \
         }                                                                              \
     } while (0)
 #define LOG_TRACE(fmt, ...)                                                            \
     do {                                                                               \
-        if (auto logger = ::Comet::Logger::get_console_logger()) {                  \
+        if (auto logger = ::Comet::Logger::get_console_logger()) {                     \
             logger->trace(fmt, ##__VA_ARGS__);                                         \
         }                                                                              \
     } while(0)
 #define LOG_FATAL(fmt, ...)                                                            \
     do {                                                                               \
-        if (auto logger = ::Comet::Logger::get_console_logger()) {                  \
+        if (auto logger = ::Comet::Logger::get_console_logger()) {                     \
             logger->critical(fmt, ##__VA_ARGS__);                                      \
         }                                                                              \
         assert(false);                                                                 \
+        std::terminate();                                                              \
     } while(0)
 }
