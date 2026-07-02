cd9d557aeec3254057ab572843804fcf53dfc26e
wsq <wsq1275093443@gmail.com>
2026-07-02 23:47:09 +0800

    fix: suppress VMA GCC warnings

--- engine/src/graphics/vma.cpp
+++ engine/src/graphics/vma.cpp
@@ -5,6 +5,10 @@
 #pragma clang diagnostic ignored "-Wnullability-completeness"
 #pragma clang diagnostic ignored "-Wunused-variable"
 #pragma clang diagnostic ignored "-Wunused-private-field"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 #endif
 
 #define VMA_IMPLEMENTATION
@@ -12,4 +16,6 @@
 
 #if defined(__clang__)
 #pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
 #endif
